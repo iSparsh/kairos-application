46245762e2b3c8af600b05470ba23da04d691479
Brian <bshuo@wisc.edu>
2024-11-09 21:47:23 -0600

    added more pages

---
+++ kairos-frontend/src/pages/settings/index.js
@@ -0,0 +1,14 @@
+
+
+export default function Page() {
+	return (
+		<>
+			<div className="card flex flex-col gap-2 w-[30em]">
+				<h1 className="w-full text-3xl text-center">this is the settings</h1>
+					
+			</div>
+
+		</>
+
+	)
+}
